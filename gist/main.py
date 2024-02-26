0662c6f7e6230449a9946d27afb889466e32f199
Fabian Mettler <fabian.mettler@axpo.com>
2024-02-26 17:45:46 +0100

    Disable doc endpoints

--- gist/main.py
+++ gist/main.py
@@ -17,7 +17,7 @@ templates = Jinja2Templates(
 
 models.Base.metadata.create_all(bind=engine)
 
-app = FastAPI()
+app = FastAPI(docs_url=None, redoc_url=None, openapi_url=None)
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
