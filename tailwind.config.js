88997665eccd8d4774bbc0bc6ed26504415eb857
Fabian Mettler <fabian.mettler@axpo.com>
2024-02-26 17:59:17 +0100

    FIX: Lookup for css classes in all HTML templates

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-  content: ["templates/*.html"],
+  content: ["templates/**/*.html"],
   theme: {
     extend: {
       fontFamily: {
