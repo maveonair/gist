64ce0bca3d0dadaefc9d2bc185e1a79820a412c5
Fabian Mettler <fabian.mettler@axpo.com>
2024-03-14 11:02:43 +0100

    Improve error handling

--- gist/database.py
+++ gist/database.py
@@ -1,3 +1,5 @@
+import logging
+
 from sqlalchemy import StaticPool, create_engine, event
 from sqlalchemy.engine.interfaces import DBAPIConnection
 from sqlalchemy.orm import declarative_base, sessionmaker
@@ -21,10 +23,10 @@ Base = declarative_base()
 def set_sqlite_pragma(
     dbapi_connection: DBAPIConnection, connection_record: ConnectionPoolEntry
 ):
-    cursor = dbapi_connection.cursor()
-
-    cursor.execute("PRAGMA journal_mode=WAL")
-    cursor.execute("PRAGMA busy_timeout = 5000")
-    cursor.execute("PRAGMA synchronous = NORMAL")
-
-    cursor.close()
+    try:
+        with dbapi_connection.cursor() as cursor:
+            cursor.execute("PRAGMA journal_mode=WAL")
+            cursor.execute("PRAGMA busy_timeout = 5000")
+            cursor.execute("PRAGMA synchronous = NORMAL")
+    except Exception as e:
+        logging.error(f"Error setting PRAGMA: {e}")
